4a1cd1b819734dca801178013835edc7bc3b2c33
Judah <schechter.ye@gmail.com>
2025-02-23 09:38:40 +0200

    Fixed a bit the layout and started on the nbavbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './components/Navbar';
+import MedicineGroup from './components/MedicineGroup';
+import { Medicine } from './types/Medicine';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+const medicines: Medicine[] = [
+  { name: "medicineA", dosage : "10m/g", timesToTake : "once per day" },
+  { name: "medicineB", dosage : "200m/g", timesToTake : "twice per day" },
+  { name: "medicineC", dosage : "2.5m/g", timesToTake : "as needed" },
+];
+
+// Placeholder Components for Routes
+const Home: React.FC = () => <div>Home Page</div>;
+const About: React.FC = () => <div>About Page</div>;
+const Services: React.FC = () => <div>Services Page</div>;
+const Contact: React.FC = () => <div>Contact Page</div>;
 
 function App() {
-  const [count, setCount] = useState(0)
+return(
+  <Router>
+    <Navbar /> {/* Render the Navbar at the top */}
+      <div className="container mt-5 pt-5">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<MedicineGroup medicines={medicines} patient='testpatient'/>} />
+        </Routes>
+      </div>
+  </Router>
+  
+);
+<div>
+    <MedicineGroup medicines={medicines} patient='testpatient'/>
+  </div>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
 }
-
 export default App
---
+++ src/components/Medicine.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Medicine = () => {
+  return (
+    <div>
+        <h1>MedicineName</h1>
+        <h1>Dosage mg</h1>
+    </div>
+  )
+}
+
+export default Medicine
